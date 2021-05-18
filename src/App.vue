5f174def434ea458d651269cafce55d127239734
tiansheng.zhu <tiansheng.zhu@amh-group.com>
2021-05-18 17:03:14 +0800

    修改：整体样式

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    <router-link to="/filter-tree">过滤tree</router-link>
+  <div style="padding: 10px">
+    <div class="link-wrapper">
+      <router-link to="/filter-tree">过滤tree</router-link>
+      <router-link to="/single-multiple-tree">单选多选tree</router-link>
+      <router-link to="/pull-down-tree">下拉tree</router-link>
+    </div>
     <router-view></router-view>
   </div>
 </template>
@@ -10,5 +14,12 @@ export default {
 </script>
 
 <style lang="stylus">
-
+.link-wrapper
+  display flex
+  padding 10px 0
+  a
+    flex 1
+    text-align center
+  a.router-link-active
+    color chartreuse
 </style>
