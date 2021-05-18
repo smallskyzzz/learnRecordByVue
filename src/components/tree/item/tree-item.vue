2edc1b2be074f36974b32b7671d7333dae23d5cc
tiansheng.zhu <tiansheng.zhu@amh-group.com>
2021-05-18 17:04:44 +0800

    新增：单选多选tree

---
+++ src/components/tree/item/tree-item.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <Checkbox v-if="!data.radio" v-model="data.checkBox" @on-change="handleCheckBox" />
+    <Button v-if="data.radio" style="position: absolute;opacity: 0;z-index: 10;width: 20px;height: 20px;padding: 0" @click="handleRadio"></Button>
+    <Radio v-if="data.radio" v-model="data.radio" />
+    {{ data.title }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "tree-item",
+    props: {
+      data: {
+        type: Object,
+        default: () => {}
+      }
+    },
+    // watch: {
+    //   'data.checkBox': {
+    //     handler (newVal, oldVal) {
+    //       if (!oldVal) {
+    //         this.data.radio = true
+    //       } else {
+    //         this.data.radio = false
+    //       }
+    //     }
+    //   }
+    // },
+    methods: {
+      handleCheckBox (e) {
+        if (e) {
+          this.data.radio = true
+        } else {
+          this.data.radio = false
+          this.$emit('changeStatus', this.data)
+        }
+      },
+      handleRadio () {
+        this.data.checkBox = true
+        this.data.radio = false
+        this.$emit('changeStatus', this.data)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/tree/single-multiple-tree.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <Tree :data="data" :render="renderTree"></Tree>
+  </div>
+</template>
+
+<script type="text/jsx">
+import TreeItem from './item/tree-item'
+
+export default {
+  name: "single-multiple-tree",
+  data () {
+    return {
+      data: [{
+        expand: true,
+        title: '一级 1',
+        checkBox: false,
+        radio: false,
+        children: [{
+          expand: true,
+          title: '二级 1-1',
+          checkBox: false,
+          radio: false,
+          children: [
+            {
+              expand: true,
+              title: '三级 1-1-2',
+              checkBox: false,
+              radio: false,
+            }
+          ]
+        }]
+      }, {
+        expand: true,
+        title: '一1级 1',
+        checkBox: false,
+        radio: false,
+        children: [{
+          expand: true,
+          title: '二1级 1-1',
+          checkBox: false,
+          radio: false,
+          children: [
+            {
+              expand: true,
+              title: '三1级 1-1-2',
+              checkBox: false,
+              radio: false,
+            }
+          ]
+        }]
+      }],
+      checkStrictly: true
+    }
+  },
+  component: {
+    TreeItem
+  },
+  methods: {
+    renderTree (h, { root, node, data }) {
+      return (
+        <TreeItem data={data} on-changeStatus={this.changeStatus} />
+      )
+    },
+    changeStatus (data) {
+      this.data = this.changeTreeData(this.data, data)
+      console.log(this.data)
+    },
+    changeTreeData (data, d) {
+      let flag = false
+      data.forEach((r) => {
+        if (r.title === d.title) {
+          flag = d.checkBox
+          data.children = this.changeTreeStatus(r.children, flag)
+          return data
+        }
+        if (r.children && r.children.length && r.children !== 'null') {
+          this.changeTreeData(r.children, d)
+        }
+      })
+      return data
+    },
+    changeTreeStatus (data, flag) {
+      console.log(data)
+      data.forEach((r, i) => {
+        r.checkBox = flag
+        if (r.children && r.children.length && r.children !== 'null') {
+          this.changeTreeStatus(r.children, flag)
+        }
+      })
+      return data
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
